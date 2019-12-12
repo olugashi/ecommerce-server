33da11393d227288ed88ab46db464044ee805973
olugashi <44696376+olugashi@users.noreply.github.com>
2019-12-12 08:02:59 +0200

    Create User (service controller and model)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+require("rootpath")();
 const express = require("express");
-const bodyParser = require("body-parser");
-
-const product = require("./routes/product.route"); // Imports routes for the products
-
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("helpers/jwt");
+const errorHandler = require("helpers/error-handler");
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+const product = require("./routes/product.route"); // Imports routes for the products
 app.use("/products", product);
+
+//app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
 
 let port = 1234;
 
